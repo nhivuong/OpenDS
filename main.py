2394744813386078459fd9699f7299bd346d19b5
Nhi Vuong <nhivuong@Nhis-MacBook-Pro.local>
2019-12-11 03:01:50 -0500

    initialized helper py functions

---
+++ main.py
@@ -0,0 +1,120 @@
+import cv2
+from matplotlib import pyplot as plt
+import numpy as np
+from scipy.ndimage.filters import maximum_filter
+import math
+from sklearn.feature_selection import mutual_info_classif
+
+from util import processNormalization, calculateFeatureMaps, norm01, getPyramid
+from featureMap import CFMcompute, CSFcompute
+
+def run(image, params):
+    b = image[:,:,0]/255.
+    g = image[:,:,1]/255.
+    r = image[:,:,2]/255.
+    I = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)/255.
+
+    b_pyr = getPyramid(b, params['max_level'])
+    g_pyr = getPyramid(g, params['max_level'])
+    r_pyr = getPyramid(r, params['max_level'])
+    I_pyr = getPyramid(I, params['max_level'])
+
+    # calculating scale-wise feature maps
+    scaledFeaturePyramids = {}
+
+    for i in range(2, len(b_pyr)):
+        p_r = r_pyr[i]
+        p_g = g_pyr[i]
+        p_b = b_pyr[i]
+        p_L = I_pyr[i]
+
+        maps = calculateFeatureMaps(p_r, p_g, p_b, p_L, params)
+
+        scaledFeaturePyramids[i] = maps
+
+
+    # calculating center surround feature maps
+    centerSurroundFeatureMaps = CSFcompute(scaledFeaturePyramids)
+
+
+    # normalizing activation maps
+    normalised_maps =[]
+    norm_maps = centerSurroundFeatureMaps.copy()
+    for i in range(0,4):
+        for mat in norm_maps[i]:
+            # Resizing to sigma = 4 maps
+            nmap = processNormalization(mat)
+            nmap = cv2.resize(nmap, (b_pyr[4].shape[1], b_pyr[4].shape[0]), interpolation=cv2.INTER_CUBIC)
+            normalised_maps.append(nmap)
+
+
+    # combine normalised maps
+    comb_maps = []
+    cfn = len(norm_maps[0])+len(norm_maps[1])
+    ifn = len(norm_maps[2])
+    ofn = len(norm_maps[3])
+
+    comb_maps.append(normalised_maps[0])
+    for i in range(1, cfn):
+        comb_maps[0] = np.add(comb_maps[0], normalised_maps[i])
+
+    comb_maps.append(normalised_maps[cfn])
+    for i in range(cfn+1, cfn + ifn):
+        comb_maps[1] = np.add(comb_maps[1], normalised_maps[i])
+
+    comb_maps.append(normalised_maps[cfn + ifn])
+    for i in range(cfn + ifn + 1, cfn + ifn + ofn):
+        comb_maps[2] = np.add(comb_maps[2], normalised_maps[i])
+
+
+    # normalise top channle maps
+    ntcmaps = [None]*3
+    for i in range(0,3):
+        ntcmaps[i] = processNormalization(comb_maps[i])
+
+    # add all of them
+    mastermap = (ntcmaps[0] + ntcmaps[1] + ntcmaps[2])/3.0
+
+    #post processing
+    gray = norm01(mastermap)
+    # blurred = cv2.GaussianBlur(gray,(3,3), 4)
+    # gray = norm01(blurred)
+    mastermap_res = cv2.resize(gray, (image.shape[1], image.shape[0]), interpolation=cv2.INTER_CUBIC)
+
+    return mastermap_res
+
+def setupParams():
+    #'stddev': 2,
+    #'elongation': 2,
+    #'filterSize': -1,
+    gaborparams = {
+        'stddev': 4,
+        'elongation': 1.0,
+        'filterSize': -1,
+        'filterPeriod': np.pi
+    }
+    
+    #'sigma_frac_act': 0.15, 
+    #'sigma_frac_norm': 0.06,
+    params = {
+        'gaborparams': gaborparams,
+        'sigma_frac_act': 0.16, 
+        'sigma_frac_norm': 0.08,
+        'max_level': 9,
+        'thetas': [0, 45, 90, 135]
+    }
+
+    return params
+
+
+if __name__ == '__main__':
+    # All rights belong to: https://github.com/shreelock/gbvs/blob/master/ittikochneibur.py 
+    params = setupParams()
+   
+    img = cv2.imread("./testimages/test3.png", 1)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) 
+
+    saliency_map = run(img, params)*255.0
+ 
+    plt.rcParams["figure.figsize"] = (15,15)
+    plt.imshow(saliency_map, cmap='gray')
---
+++ util.py
@@ -0,0 +1,107 @@
+
+import cv2
+import numpy as np
+from scipy.ndimage.filters import maximum_filter
+
+from featureMap import CFMcompute, CSFcompute
+from GaborKern import getGaborKernels
+
+def calculate(mat, thresh):
+    (w,h) = mat.shape
+    sum_local_max = mat[0][0]
+    count_local_max = 0
+    global_max = mat[0][0]
+    for i in range(1, w-1):
+        for j in range(1, h-1):
+            if mat[i][j] > max(mat[i-1][j-1],mat[i-1][j],mat[i-1][j+1],
+                               mat[i][j-1],              mat[i][j+1],
+                               mat[i+1][j-1],mat[i+1][j],mat[i+1][j+1]) and mat[i,j]>thresh:
+                if mat[i][j] > global_max:
+                    global_max = mat[i][j]
+
+                sum_local_max += mat[i][j]
+                count_local_max +=1
+
+    if count_local_max > 0:
+        local_max_avg = float(sum_local_max)/float(count_local_max)
+    else:
+        local_max_avg = 0.0
+    return global_max, count_local_max, local_max_avg
+
+
+def processNormalization(mat):
+    #M = 10
+    M=90
+    thresh  = M/10
+    mat = cv2.normalize(mat, None, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
+    mat = mat*M
+    g_max, c_max, l_max_avg = calculate(mat, thresh)
+ 
+    if c_max>1:
+        res = mat* (M-l_max_avg)**2
+    elif c_max == 1:
+        res = mat * M**2
+    else:
+        res = mat
+
+    return res
+
+def process2(mat):
+    #M = 8.0 # an arbitrary global maxima for which the image is scaled
+    M = 8.0
+    mat = cv2.convertScaleAbs(mat, alpha=M/mat.max(), beta = 0.0)
+    w, h = mat.shape
+    maxima = maximum_filter(mat, size=(1, 1))
+    maxima = (mat == maxima)
+    mnum = maxima.sum()
+    maxima = np.multiply(maxima, mat)
+    mbar = float(maxima.sum()) / mnum
+    return mat * (M-mbar)**2
+
+
+def OFMcompute(L, gaborparams, thetas):
+    # L = Intensity Map
+    # L = np.maximum(np.maximum(r, g), b)
+
+    kernels = getGaborKernels(gaborparams, thetas)
+    featMaps = []
+    for th in thetas:
+        kernel_0  = kernels[th]['0']
+        kernel_45 = kernels[th]['45']
+        kernel_90 = kernels[th]['90']
+        kernel_135 = kernels[th]['135']
+        
+        o1 = cv2.filter2D(L, -1, kernel_0, borderType=cv2.BORDER_REPLICATE)
+        o2 = cv2.filter2D(L, -1, kernel_45, borderType=cv2.BORDER_REPLICATE)
+        o3 = cv2.filter2D(L, -1, kernel_90, borderType=cv2.BORDER_REPLICATE)
+        o4 = cv2.filter2D(L, -1, kernel_135, borderType=cv2.BORDER_REPLICATE)
+        p1 = np.add(abs(o1), abs(o2))
+        p2 = np.add(abs(o3), abs(o4))
+        o = np.add(abs(p1), abs(p2))
+        featMaps.append(o)
+
+    return featMaps
+
+def norm01(mat):
+    return cv2.normalize(mat, None, alpha=0., beta=1., norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
+
+def calculateFeatureMaps(r, g, b, L, params):
+    colorMaps = CFMcompute(r, g, b, L)
+    orientationMaps = OFMcompute(L, params['gaborparams'] , params['thetas'])
+    allFeatureMaps = {
+        0: colorMaps[0],
+        1: colorMaps[1],
+        2: colorMaps[2],
+        3: orientationMaps
+    }
+    return allFeatureMaps
+
+def getPyramid(image, max_level):
+    imagePyramid = {
+        0: image
+    } # scale zero = 1:1
+
+    for i in range(1, max_level):
+        imagePyramid[i] = cv2.pyrDown(imagePyramid[i-1])
+
+    return imagePyramid
